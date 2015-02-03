77a8774871dc7d8d4de28e1f440cbcf0ddbe0a28
wxu <winnielandau@gmail.com>
2015-02-02 18:23:51 -0800

    delete icon has opacity 0.3 but transitions to 1 when mouse hover on the song item

--- wxu.js
+++ wxu.js
@@ -12,18 +12,21 @@ When a song is hovered, animate its background color to a lighter grey over 0.5s
  
     $(this).parent().slideUp(500, function() {
       $(this).remove();  //call is on $(this) because the parent "LI" is the object passed to remove() already.
-      });
     });
+  });
  
-    $('ul li div').hide();
-    $('li').dblclick(function(){
-      $(this).find('div').slideDown(300);
-    });
+  $('ul li div').hide();
+  $('li').dblclick(function(){
+    $(this).find('div').slideDown(300);
+  });
 
-    $('#message').fadeIn(800, function(){
-      setTimeout(function(){
-        $('#message').fadeOut(800);
-      }, 3000);
-    });
+  $('#message').fadeIn(800, function(){
+    setTimeout(function(){
+      $('#message').fadeOut(800);
+    }, 3000);
+  });
+
+  //$('.fa-trash').css('opacity', '0.3');
+
 
 });
