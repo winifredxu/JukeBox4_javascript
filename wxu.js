1a49456b4ed761f96e243e4078d30d1b7b6a2e43
wxu <winnielandau@gmail.com>
2015-02-02 18:49:01 -0800

    load jQueryUI, add sortable to playlist-list and library-list

--- wxu.js
+++ wxu.js
@@ -6,8 +6,7 @@ When the delete icons for a song are clicked slide up that song over 0.5s. AFTER
 Make the song notes hidden when the page initially loads. Then, when you double click a song, they should slide down over 0.3 second. (Hint: See the dblclick event type)
 When the page loads, make the message fade in over 0.8s. Then, after 3s have passed, fade out the message over 0.8s.
 Make the songs' delete icons have a default opacity of 0.3. When your mouse hovers over a song, animate its delete icon's opacity to 1 using CSS transitions. (Hint: The ":hover" CSS Selector may be helpful)
-When a song is hovered, animate its background color to a lighter grey over 0.5s, using CSS selectors.
-*/
+When a song is hovered, animate its background color to a lighter grey over 0.5s, using CSS selectors.  */
   $(document).on('click', '.fa-trash', function(){
  
     $(this).parent().slideUp(500, function() {
@@ -26,7 +25,14 @@ When a song is hovered, animate its background color to a lighter grey over 0.5s
     }, 3000);
   });
 
-  //$('.fa-trash').css('opacity', '0.3');
+/* Part 2 --
+Add a script tag to load jQuery UI javascript on the page, using Google's CDN (google: "jQuery UI google CDN").
+Make the Library and Playlist sortable.
+Allow dragging and dropping songs between the Library and the Playlist. */
 
-
+  $(function() {
+    $( "#playlist-list, #library-list" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  });
 });
